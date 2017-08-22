3446cc28ed3e7c18d1f5595ca2b90ac1e29232b5
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-08-22 08:51:22 +0200

    js gets all employees and displays corrrectly

--- js/main.js
+++ js/main.js
@@ -7,18 +7,47 @@ $.ajax({
   success: function(data) {
   	//log the entire object to the console
     console.log(data);
+    //get array of all first names
+    let firstNames = [];
+    for (let i = 0; i < data.results.length; i++) {
+      firstNames.push(data.results[i].name.first);
+    }
+    //get array of all last names
+    let lastNames = [];
+    for (let i = 0; i < data.results.length; i++) {
+      lastNames.push(data.results[i].name.last);
+    }
+    //get array of all imgs
+    let imgs = [];
+    for (let i = 0; i < data.results.length; i++) {
+      imgs.push(data.results[i].picture.large);
+    }
+    //get array of all emails
+    let emails = [];
+    for (let i = 0; i < data.results.length; i++) {
+      emails.push(data.results[i].email);
+    }
+    //get array of all cities
+    let cities = [];
+    for (let i = 0; i < data.results.length; i++) {
+      cities.push(data.results[i].location.city);
+    }
+
     //create the employee widget
-    let employee = '<div class="employee">';
-    employee += '<div class="employee-img">';
-    employee += '<img alt="" src="' + data.results[0].picture.large + '">';
-    employee += '</div>'; //end the img container
-    employee += '<div class="employee-information">';
-    employee += '<p class="employee-name">' + data.results[0].name.first + data.results[0].name.last + '</p>';
-    employee += '<p class="employee-email">' + data.results[0].email + '</p>';
-    employee += '<p class="employee-city">' + data.results[0].location.city + '</p>';
-    employee += '</div>';//end employee-information div
-    employee += '</div>';//end employee div
+    let employee = "";
+    for (let i = 0; i < data.results.length; i++) {
+      employee += '<div class="employee employee-' + i + '">';
+      employee += '<div class="employee-img">';
+      employee += '<img alt="" src="' + imgs[i] + '">';
+      employee += '</div>'; //end the img container
+      employee += '<div class="employee-information">';
+      employee += '<p class="employee-name">' + firstNames[i] + " " + lastNames[i] + '</p>';
+      employee += '<p class="employee-email">' + emails[i] + '</p>';
+      employee += '<p class="employee-city">' + cities[i] + '</p>';
+      employee += '</div>';//end employee-information div
+      employee += '</div>';//end employee div
+    }
 
     $('.employees').append(employee);
   	}
-});
+}); //end AJAX
